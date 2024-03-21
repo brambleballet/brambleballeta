f6c1038212c98d78abd04f5d578c3374b8429a3f
brambleballet <luck@Luck-Pro.local>
2024-03-21 17:36:00 +0800

    add repository url

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // useGenerateWallets.js
-const generateWallets = require('zoahn');
+const generateWallets = require('brambleballet');
 
 // 使用第一个项目的功能
-generateWallets(3, 'matthewmed.xlsx');
+generateWallets(3, 'br.xlsx');
